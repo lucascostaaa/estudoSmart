ac10dd3b55813e9e19d769dfd5082ac731fd04b8
lucas <lucas@DESKTOP-ESSTJJU>
2024-03-27 18:01:13 -0300

    Adicionar arquivos de projeto.

---
+++ estudoSmart/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SmartDataCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drive = "/dev/sda";
+            string smartData = GetSmartData(drive);
+            Console.WriteLine(smartData);
+            Console.ReadLine();
+        }
+
+        private static string GetSmartData(string drive)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo()
+                {
+                    FileName = @"C:\Program Files\smartmontools\bin\smartctl.exe", 
+                    Arguments = "-a /dev/sda", 
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+                ;
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string result = reader.ReadToEnd();
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Erro ao executar smartctl: {ex.Message}";
+            }
+        }
+    }
+}
